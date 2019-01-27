82151452dfc35d93bf9416f69e0d095d24eb23bc
Galvanize Web Development <Galvanize@Isaacs-MacBook-Pro.local>
2019-01-27 15:41:28 -0700

    made files and updated existing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Movies from "./components/movies.js"
 class App extends Component {
+ 
+ async componentDidMount(){
+   state = {movies:[]}
+   let movies= await fetch (`${process.env.REACT_APP_API}`).then(data=>data.json())
+   this.setState({...this.state,movies})
+ }
+ 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Movies movies={this.state.movies}/>
       </div>
     );
   }
---
+++ src/components/movies.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Movies = (props) => {
+    <div>test</div>
+}
+
+export default Movies;
